4aa389baabf108a04dc31b6f11df55655f9c1a42
Radu Stoica <rstoica@yellowtail.nl>
2018-09-23 13:19:07 +0200

    added core graphql project

--- models/ING.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var IngSchema = new Schema({
-    amount: { type: Number, required: true },
-    timestamp: { type: Date, required: true }
-});
-
-module.exports = IngSchema;
